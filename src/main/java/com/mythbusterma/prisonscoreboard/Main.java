e6a0ce992c18b441d3461d90781a4fdceabb23af
mythbusterma <wiirocksme@gmail.com>
2014-09-14 21:07:38 -0400

    more vault

--- src/main/java/com/mythbusterma/prisonscoreboard/Main.java
+++ src/main/java/com/mythbusterma/prisonscoreboard/Main.java
@@ -1,8 +1,13 @@
 package com.mythbusterma.prisonscoreboard;
 
+import net.milkbowl.vault.permission.Permission;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+
+    public Permission permission = null;
+
     @Override
     public void onEnable() {
 
@@ -11,6 +16,19 @@ public class Main extends JavaPlugin {
     @Override
     public void onDisable() {
 
+    }
+
+    private boolean setupPermissions()
+    {
+        RegisteredServiceProvider<Permission> permissionProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.permission.Permission.class);
+        if (permissionProvider != null) {
+            permission = permissionProvider.getProvider();
+        }
+        return (permission != null);
+    }
+
+    public Permission getPermission () {
+        return permission;
     }
 
 }
